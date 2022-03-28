34f631a3c718f855efe75031c54c04a387572e73
Anirudh P Preeth <anirudhpreeth@gmail.com>
2022-03-28 15:25:54 +0530

    Create program2.py

---
+++ Python GeekforGeeks/program2.py
@@ -0,0 +1,9 @@
+# Program to add two numbers by user input. The
+
+number1 = input("First Number : ")
+number2 = input("Second Number : ")
+
+sum = float(number1) + float(number2)
+
+print("the sum of {0} and {1} is {2}" .format(number1, number2, sum))
+
