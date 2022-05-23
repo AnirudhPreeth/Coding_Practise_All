1880050ef05e5af189cfc590d9ecd2db206d9977
Anirudh P Preeth <anirudhpreeth@gmail.com>
2022-05-23 23:31:32 +0530

    Update

---
+++ C - 101 programs/program1.c
@@ -0,0 +1,28 @@
+/*
+Program 1 - Kodegod.com.
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf("\nKodegod.com");
+    return 0;
+}
+
+/*
+Format Specifiers ->%d - which is Integer, %c - Char single character, %f - Float or double single dec.
+                    %e(%E) - Float or double exponential format, %o - int unsigned octal value, 
+                    %g(%G) - Float or double use %f or %e as required,
+                    %p - Pointer address stored in pointer, 
+                    %s - Array of characters sequence of characters, 
+                    %u - Int unsigned decimal, %x(%X) - Int unsigned hex value.
+
+Printf -> Input. Scanf -> Output.  
+
+Clrscr -> Previous executions will get cleared. 
+Getch -> Time to check the final output of the program. Till the time you don't enter any character..
+         ...on the keyboad, the complier will wait and the program will get not get terminated. Once...
+         ...the character is entered, the program will get terminated. 
+         
+[Void main also works, but I prefer Int Main for the time being.]
+*/
---
+++ C - 101 programs/program2.c
@@ -0,0 +1,17 @@
+/*
+
+*/
+
+
+/*
+Variables - Must indicate what the value it stores.
+            Ideally should be assigned some values at the time of declaration. 
+            Must not contain any special character, but underscore is fine. 
+            Must start with an alphabet or underscore.
+            Must not be a keyword. 
+
+Valid -> int roll_number, float pivalue, char o, int_Superkul.
+Invalid -> int roll_number, float 3.142, char 0, int if. 
+
+
+*/
