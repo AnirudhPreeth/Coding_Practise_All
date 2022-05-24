40639910959a40c6458e769fc41646c90640dffe
Anirudh P Preeth <anirudhpreeth@gmail.com>
2022-05-24 14:56:05 +0530

    Python

---
+++ Python - Corey Schafer/video1.py
@@ -0,0 +1,31 @@
+"""
+Video 1. 
+"""
+#Print Welcome Message.
+print("Hello World.")
+
+x = 10
+print(x)
+
+
+"""
+Installation.
+
+PKG file. 
+IDLE. 
+
+Windows - cmd (Command Prompt)
+python --version -> Error, then python not installed yet. 
+Click "Add Python to PATH".
+
+Interactive Prompt -> One line of python at a time. 
+To exit Interactive Prompt -> exit()
+
+IDLE -> Another Interactive Prompt. 
+
+Single inline comment -> # 
+
+Editors : Sublime Text, Atom. 
+IDE : PyCharm. 
+These videos use Sublime Text.
+"""
